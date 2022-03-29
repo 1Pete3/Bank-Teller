ed6a8bb5f53cf5872d0c8eb2b0db396fe28f8b32
1Pete3 <skowronekpeter103@gmail.com>
2022-03-29 04:35:54 +0000

    Added Multiple different transactions, Use command line to select

--- BankTeller.cpp
+++ BankTeller.cpp
@@ -1,14 +1,28 @@
 // Hello GOOGLE
+
+
 #include <iostream>
+#include<string.h>
 using namespace std;
 
+//command line arguments for bank transaction
 int main(int argc, char** argv)
 {
-	cout << "You have entered " << argc
-		<< " arguments:" << "\n";
-
-	for (int i = 0; i < argc; ++i)
-		cout << argv[i] << "\n";
-
+    // selects type of transaction
+	if (argc == 2 && strcmp(argv[1],"deposit")==0){
+        printf("Deposit Selected\n");
+    }
+    else if(argc == 2 &&strcmp(argv[1],"widthdraw")==0)
+    {
+        printf("Widthdraw Selected\n");
+    }
+    else if(argc == 2 &&strcmp(argv[1],"balance")==0)
+    {
+        printf("Balance Selected\n");
+    }
+    else if(argc == 2 &&strcmp(argv[1],"overdraft")==0)
+    {
+        printf("overdraft Selected\n");
+    }
 	return 0;
 }
