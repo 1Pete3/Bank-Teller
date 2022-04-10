4c07154646176f5ed831d82f08c7d39559971272
1pete3 <skowronekpeter103@gmail.com>
2022-04-10 02:30:18 +0000

    created a deposit function

--- BankTeller.cpp
+++ BankTeller.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include<string.h>
 using namespace std;
-
+//test
 // Function that asks for user input for the account number, first name, last name
 void getInfo(){
     char fName [15], lName[20], accountNumber[12];
 
-    cout << "Enter the customners first name: ";
+    cout << "Enter the customers first name: ";
     cin >> fName;
 
-    cout << "Enter the customners last name: ";
+    cout << "Enter the customers last name: ";
     cin >> lName;
 
     // can change this to int to show vulnerability if account number > max int value etc..
-    cout << "Enter the customners account #: ";
+    cout << "Enter the customers account #: ";
     cin >> accountNumber;
     
+}
+
+void deposit()
+{
+    char depositType [6];
+    cout << "Is this a cash or check deposit?: ";
+    cin >> depositType;
+    if (strcmp("cash",depositType)==0) {
+    cout << "cash deposit" << endl;
+    }    
+    else if(strcmp("check",depositType)==0)
+    cout << "check deposit" << endl;
 }
 
 //command line arguments for bank transaction
@@ -25,21 +37,29 @@ int main(int argc, char** argv)
 	if (argc == 2 && strcmp(argv[1],"deposit")==0){
         printf("Deposit Selected\n");
         getInfo();
+        deposit();
+
     }
     else if(argc == 2 &&strcmp(argv[1],"widthdraw")==0)
     {
-        printf("Widthdraw Selected\n");
+        cout << "Widthdraw Selected" <<endl;
         getInfo();
     }
     else if(argc == 2 &&strcmp(argv[1],"balance")==0)
     {
-        printf("Balance Selected\n");
+        cout << "Balance Selected"<<endl;
          getInfo();
     }
     else if(argc == 2 &&strcmp(argv[1],"overdraft")==0)
     {
-        printf("overdraft Selected\n");
+        cout <<"overdraft Selected"<<endl;
         getInfo();
+    }
+    else {
+    cout << "Use ./a.out deposit for a deposit" <<endl;
+    cout << "Use ./a.out widthdraw for a widthdraw" <<endl;
+    cout << "Use ./a.out balance for a balance inquiry" <<endl;
+    cout << "Use ./a.out overdraft to apply an overdraft fee" <<endl;
     }
 	return 0;
 }
