b70dab7c4e0292e5912886e227521e8dd9764be5
1pete3 <skowronekpeter103@gmail.com>
2022-04-25 22:44:55 +0000

    Rewrote the whole project to be more organized

--- BankTeller.cpp
+++ BankTeller.cpp
@@ -1,3 +1,4 @@
+#include<bits/stdc++.h>
 #include <cctype>
 #include <cstdio>
 #include <iostream>
@@ -13,78 +14,107 @@
 #include <cctype>
 #define EST (-4)
 using namespace std;
-
-char test[5];
-int createAccount(),deleteAccount();
-void continueExit(),checkInputSize(char* input,int size);
-
-vector<unsigned int> accountNums;
-
+int NumberofAccounts=1;
+int userInput;
 fstream account;
+void continueExit();
+void checkInputSize(char* input,int size );
+
+
+vector< unsigned int> accountNumbers;
 time_t rawtime;
 struct tm * ptm;
-
 class Person
 {
-    
-    private: 
-    static const int size20 = 21;
-    static const int sizeState = 3;
-    char fName [size20], lName[size20],houseNumStreetName[size20],town[size20],state[sizeState],zipcode[6];
+    private:
+    const static int size = 21;
+    char fName [size],lName[size],state[3],zipcode[6];
+    string houseNumStreetName,town;
+    public:
+    unsigned int accountNumber;
+    Person()
+    {
+    } 
 
-    public: 
-    /* *Buffer Overrun* can occur here if the user enters a string greater than the size of
-            fName, lName
-        */
+    ~Person()
+    {
+    }
+
+    unsigned int generateAccountNumber()
+    {
+        random_device r;
+        seed_seq seed {r(), r(), r(), r(), r(), r(), r(), r(), r(), r()};
+        mt19937 engine{seed};
+        // range is from 1000000000 - INT_MAX
+        uniform_int_distribution<> dist(1000000000, INT_MAX);
+        accountNumber = dist(engine);  
+        for(int i = 0; i < accountNumbers.size();i++)
+        {
+            if(accountNumbers[i]==accountNumber)
+            {                
+                generateAccountNumber();
+                cout << "same account num generating new one"<<endl;
+            }
+            else if(NumberofAccounts == (INT_MAX-1000000000 + 1))
+            {
+                cout<<"Max accounts created"<<endl;
+                exit(0);
+            }
+        }
+        return accountNumber;
+    }
     string getFirstName() 
     {
         cout << "Enter the customers first name: ";
-        cin >> fName;  
-        checkInputSize(fName,size20);      
+        cin >> fName;    
+        checkInputSize(fName, size);  
         return fName;
     }
+
     string getLastName() 
     {
         cout << "Enter the customers last name: ";
         cin >> lName;
-        checkInputSize(lName, size20);        
+        checkInputSize(fName, size);         
         return lName;
     }
     string getHouseNumStreet()
     {
         cout << "Enter the house number and street name : ";
-        // use ignore() to clear buffer so that cin is not skipped
+        // use ignore() so that cin is not skipped
         cin.ignore();
-        cin.getline(houseNumStreetName,size20);
+        getline(cin,houseNumStreetName);
         return houseNumStreetName;
-                
-        
     }
     string getTown()
     {        
         cout << "Enter the town: ";
-        cin.getline(town,size20);
+        getline(cin,town);
         return town;
     }
     string getState()
     {
         cout << "Enter the state (2 letter abbrivation): ";
         cin >> state;
+        checkInputSize(state, 3);
         // Capitalizes the state entered
         for(int i = 0; i < strlen(state);i++)
         {
            state[i] = toupper(state[i]);
         }
+        
         return state;
     }
     string getZipcode()
     {
         cout << "Enter the zipcode: ";
         cin >> zipcode;
+        checkInputSize(zipcode, 6);
         return zipcode;
     }
 };
 
+// Checks for buffer over flow 
 void checkInputSize(char* input,int size )
     {
         while(strlen(input) >= size )
@@ -95,10 +125,10 @@ void checkInputSize(char* input,int size )
 
     }
 
-// terminal colors - https://man7.org/linux/man-pages/man5/terminal-colors.d.5.html
-// https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
-// Function to create a menu 
-void menu()
+
+ 
+
+int menu() 
 {
     int menuSelection;
     cout<<"|=============================|"<< endl;
@@ -112,140 +142,65 @@ void menu()
     cout<<"| 6.Apply fee"<<"                 |"<< endl;
     cout<<"| 7.Exit"<<"                      |"<< endl;
     cout<<"==============================="<< endl;
+    
     cout << "Enter a menu item number: ";
     cin >> menuSelection;
 
-    time ( &rawtime );
-    ptm = gmtime ( &rawtime );
-    switch(menuSelection)
+    
+    if(menuSelection == 1)
     {
-        case 1: 
-            createAccount();
-            break;
-        case 2: 
-            deleteAccount();
-            break;
-        case 3: 
-            createAccount();
-            break;
-        case 4: 
-            createAccount();
-            break;
-        case 5: 
-            createAccount();
-            break;
-        case 6: 
-            createAccount();
-            break;
-        case 7:
-            cout << "Goodbye :)"<<endl;
-            exit(0);
-        
-    }
-
-}
-
-// Taken from the inclass example of generating random numbers
-    unsigned int generateAccountNumber(){
-    unsigned int accountNumber;
-    random_device r;
-    seed_seq seed {r(), r(), r(), r(), r(), r(), r(), r(), r(), r()};
-    mt19937 engine{seed};
-    // range is from 1000000000 - INT_MAX
-    uniform_int_distribution<> dist(1000000000, INT_MAX);
-    accountNumber = dist(engine);
-    return accountNumber;
-};
-
-
-//Creates a csv file with the account information
-int createAccount()
-{
-    Person obj;
-    //open accounts.csv if it exists append to it, else create a new csv called account.csv
-    account.open("account.csv");
-    if(account)
-    {
-        cout << "File account.csv exits, appending to it" << endl;          
-        account.close();
-        account.open("account.csv",std::ios::app);
-    }
-    else 
-    {
-        cout << "Creating file account.csv" << endl;
-        account.open("account.csv", std::ios::out);
-        if(!account)
-        {
-            cout<<"Error in creating file!!!";
-            return 0;
-        }
-        else 
-        {
-            account << "Firstname,Lastname,Account#,Address,Town,State,Zipcode,Time(EST),Date"<<endl;
-        }   
-    } 
-    account << obj.getFirstName();
-    account << ",";
-    account << obj.getLastName();
-    account << ",";
-    account << generateAccountNumber();
-    account << ",";
-    account << obj.getHouseNumStreet();
-    account << ",";
-    account << obj.getTown();
-    account << ",";
-    account << obj.getState();
-    account << ",";
-    account << obj.getZipcode();
-    account << ",";
-    //Time formated in HH:MM:SS using setw to set width to 2 and fill 0 where needed
-    account << setw(2) << setfill('0') << ((ptm->tm_hour+EST)%24+24)%24 <<":" << setw(2) << setfill('0') << ptm->tm_min<<":" << setw(2) << setfill('0') << ptm->tm_sec;
-    account << ",";
-    account << setw(2) << setfill('0') << (ptm->tm_mon+1)%12 <<"/" << setw(2) << setfill('0') << ptm->tm_mday<<"/" << setw(4) << setfill('0') << ptm->tm_year+1900 ;
-    account << "\n";
-    cout<<"File appended successfully."<<endl;
-    account.close();
-    continueExit();
-   return 0;
-}
-
-int deleteAccount()
-{
-    account.open("account.csv");
-    string line = "";
-    while (getline(account,line)) {
-        stringstream accountData(line);
-        line = "";
-    }
-    return 0;
-}
-
-
-
-void deposit()
-{
-    char depositType [6];
-    do{
-        cout << "Is this a cash or check deposit?: ";
-        // *Buffer Overrun* can occur here if the user enters a string greater than the size of depositType 
-        cin >> depositType;
-        if (strcmp("cash",depositType)==0) {
-            cout << "Cash deposit selected" << endl;
-            cout << "Amount of cash deposited: ";
+        account.open("account.csv");
+        time ( &rawtime );
+        ptm = gmtime ( &rawtime );
+            if(account)
+            {
+                cout << "File account.csv exits, appending to it" << endl;          
+                account.close();
+                account.open("account.csv",std::ios::app);
             }
-        else if (strcmp("check",depositType)==0)
-        {
-            cout << "check deposit selected" << endl;
-            cout << "Amount of cash deposited: ";
-        }    
-        else {
-            // \033 is to change the text color, [31 - red text, 103m - yellow background for linux
-            // \033[0m is to reset the text color/background             
-            cout << "\033[31;103mIncorrect Input enter cash or check\033[0m" << endl;
-        }
-            
+            else 
+            {
+                cout << "Creating file account.csv" << endl;
+                account.open("account.csv", std::ios::out);
+                if(!account)
+                {
+                    cout<<"Error in creating file!!!";
+                    return 0;
+                }
+                else 
+                {
+                    account << "Account#,Firstname,Lastname,Address,Town,State,Zipcode,Time(EST),Date"<<endl;
+                }   
+            } 
+
+        Person* person = new Person();
+        accountNumbers.push_back(person->generateAccountNumber());
+        account << person->accountNumber;
+        account << ",";
+        account << person->getFirstName();
+        account << ",";
+        account << person->getLastName();
+        account << ",";
+        account << person->getHouseNumStreet();
+        account << ",";
+        account << person->getTown();
+        account << ",";
+        account << person->getState();
+        account << ",";
+        account << person->getZipcode();
+        account << ",";
+        //Time formated in HH:MM:SS using setw to set width to 2 and fill 0 where needed
+        account << setw(2) << setfill('0') << ((ptm->tm_hour+EST)%24+24)%24 <<":" << setw(2) << setfill('0') << ptm->tm_min<<":" << setw(2) << setfill('0') << ptm->tm_sec;
+        account << ",";
+        account << setw(2) << setfill('0') << (ptm->tm_mon+1)%12 <<"/" << setw(2) << setfill('0') << ptm->tm_mday<<"/" << setw(4) << setfill('0') << ptm->tm_year+1900 ;
+        account << "\n";
+        account.close();
+        cout<<NumberofAccounts++;
+        cout << "\033[32mAccount created successfully\033[0m" << endl;
+        delete person; 
+        continueExit();
     }
-    while((strcmp("cash",depositType)!=0)&&(strcmp("check",depositType)!=0));
+  return 0;
 }
 
 void continueExit()
@@ -257,11 +212,9 @@ void continueExit()
     if(continueSelection == 1)
         menu();
     else
-        cout << "Goodbye :)\n"<<endl;
+        cout << "Goodbye :)\n"<<endl; 
         exit(0);
-
 }
-
 
 //command line arguments for bank transactions
 // *Command Injection* can occur here if the user adds ; after deposit or any other transaction
@@ -271,7 +224,7 @@ int main(int argc, char** argv)
 	if (argc == 2 && strcmp(argv[1],"deposit")==0){
         printf("Deposit Selected\n");
         Person obj;
-        deposit();
+        //deposit();
 
     }
     else if(argc == 2 &&strcmp(argv[1],"widthdraw")==0)
@@ -294,8 +247,8 @@ int main(int argc, char** argv)
     }
     else if(argc == 3&&strcmp(argv[1],"create")==0)
     {
-        createAccount();
-        generateAccountNumber();
+        //createAccount();
+        //generateAccountNumber();
     }
     else if(argc == 2 &&strcmp(argv[1],"delete account")==0)
     {
